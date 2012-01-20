5f7dc74c9c66281487d87fda781e244430eae343
Alexander Bruy <alexander.bruy@gmail.com>
2012-01-20 13:42:54 +0200

    added Makefile and readme (still empty) updated license header if sources

--- displacementdialog.py
+++ displacementdialog.py
@@ -2,26 +2,26 @@
 
 #******************************************************************************
 #
-# GenPoints
+# Point Displacement
 # ---------------------------------------------------------
-# Generate regular points inside polygon
+# Point displacement plugin
 #
 # Copyright (C) 2011 Alexander Bruy (alexander.bruy@gmail.com)
 #
 # This source is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
+# Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
 #
 # This code is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # A copy of the GNU General Public License is available on the World Wide Web
-# at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
-# to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-# MA 02111-1307, USA.
+# at <http://www.gnu.org/licenses/>. You can also obtain it by writing
+# to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+# MA 02110-1335 USA.
 #
 #******************************************************************************
 
