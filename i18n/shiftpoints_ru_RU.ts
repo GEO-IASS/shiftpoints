cb9a13265c1408a21fb56ce569633a49ba26c5d1
Alexander Bruy <alexander.bruy@gmail.com>
2012-01-22 12:50:39 +0200

    better wording for GUI option translations update

--- i18n/shiftpoints_ru_RU.ts
+++ i18n/shiftpoints_ru_RU.ts
@@ -79,12 +79,12 @@ original position.</source>
     <message>
         <location filename="shiftpointsdialogbase.ui" line="20"/>
         <source>Input layer</source>
-        <translation>Исходный слой</translation>
+        <translation type="obsolete">Исходный слой</translation>
     </message>
     <message>
         <location filename="shiftpointsdialogbase.ui" line="30"/>
         <source>Output vector</source>
-        <translation>Выходной файл</translation>
+        <translation type="obsolete">Выходной файл</translation>
     </message>
     <message>
         <location filename="shiftpointsdialogbase.ui" line="42"/>
@@ -99,49 +99,99 @@ original position.</source>
     <message>
         <location filename="shiftpointsdialog.py" line="68"/>
         <source>No output file selected</source>
-        <translation>Не выбран выходной файл</translation>
+        <translation type="obsolete">Не выбран выходной файл</translation>
     </message>
     <message>
         <location filename="shiftpointsdialog.py" line="68"/>
         <source>Please specify output filename.</source>
-        <translation>Укажите выходной файл.</translation>
+        <translation type="obsolete">Укажите выходной файл.</translation>
     </message>
     <message>
         <location filename="shiftpointsdialog.py" line="75"/>
         <source>Delete error</source>
-        <translation>Ошибка удаления</translation>
+        <translation type="obsolete">Ошибка удаления</translation>
     </message>
     <message>
-        <location filename="shiftpointsdialog.py" line="75"/>
+        <location filename="shiftpointsdialog.py" line="81"/>
         <source>Can&apos;t delete file %1</source>
         <translation>Не получилось удалить файл %1</translation>
     </message>
     <message>
-        <location filename="shiftpointsdialog.py" line="90"/>
+        <location filename="shiftpointsdialog.py" line="96"/>
         <source>Cancel</source>
         <translation>Отмена</translation>
     </message>
     <message>
-        <location filename="shiftpointsdialog.py" line="108"/>
+        <location filename="shiftpointsdialog.py" line="115"/>
         <source>Error loading output shapefile:
 %1</source>
         <translation>Ошибка при открытии выходного shape-файла:
 %1</translation>
     </message>
     <message>
-        <location filename="shiftpointsdialog.py" line="124"/>
+        <location filename="shiftpointsdialog.py" line="134"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="shiftpointsdialog.py" line="267"/>
-        <source>Save output shapefile</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="shiftpointsdialog.py" line="108"/>
         <source>Shift Points</source>
-        <translation>Shift Points</translation>
+        <translation type="obsolete">Shift Points</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialogbase.ui" line="20"/>
+        <source>Input vector layer</source>
+        <translation>Исходный векторный слой</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialogbase.ui" line="30"/>
+        <source>Output file</source>
+        <translation>Выходной файл</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialogbase.ui" line="72"/>
+        <source>Displacement distance</source>
+        <translation>Расстояние смещения</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialogbase.ui" line="89"/>
+        <source>Place points horizontally if count == 2</source>
+        <translation type="obsolete">Располагать точки горизонтально, когда их две</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="115"/>
+        <source>Shift Points: Error</source>
+        <translation>Shift Points: Ошибка</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="69"/>
+        <source>No output file selected. Please specify output filename.</source>
+        <translation>Не выбран выходной файл. Пожалуйста, укажите имя выходного файла.</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="74"/>
+        <source>Displacement distance is set to 0.0. Please specify correct value.</source>
+        <translation>Расстояние смещения установлено в 0.0. Пожалуйста, задайте корректное значение.</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="149"/>
+        <source>Analyze: %p%</source>
+        <translation>Анализ: %p%</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="150"/>
+        <source>Displacement: %p%</source>
+        <translation>Смещение: %p%</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialog.py" line="279"/>
+        <source>Select output shapefile</source>
+        <translation>Укажите выходной файл</translation>
+    </message>
+    <message>
+        <location filename="shiftpointsdialogbase.ui" line="89"/>
+        <source>For two point case distribute horizontally (otherwise vertically)</source>
+        <translation>В случае двух точек размещать горизонтально (иначе вертикально)</translation>
     </message>
 </context>
 </TS>
