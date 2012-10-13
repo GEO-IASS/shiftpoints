b15e57303ca4f21249cce8610f36a73d67d33237
Alexander Bruy <alexander.bruy@gmail.com>
2012-10-13 15:55:50 +0300

    adjust metadata to latest changes

--- __init__.py
+++ __init__.py
@@ -41,8 +41,11 @@ def version():
 def qgisMinimumVersion():
   return "1.0.0"
 
-def authorName():
+def author():
   return "Alexander Bruy (NextGIS)"
+
+def email():
+  return "alexander.bruy@gmail.com"
 
 def icon():
   return "icons/shiftpoints.png"
